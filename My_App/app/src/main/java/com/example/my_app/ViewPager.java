f13b45ab9cee1d3fd40267a356dbdfe2354773ff
MeiNiao <1543231665>
2019-04-02 18:15:20 +0800

    轮播

--- My_App/app/src/main/java/com/example/my_app/ViewPager.java
+++ My_App/app/src/main/java/com/example/my_app/ViewPager.java
@@ -1,213 +1,183 @@
 package com.example.my_app;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.os.SystemClock;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.AbsListView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.my_app.Adapter.ViewPagerAdapter;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.common.ResizeOptions;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ViewPager extends AppCompatActivity {
-    private ViewPager mViewPager;
+public class ViewPager extends AppCompatActivity implements View.OnClickListener {
+    // 图片轮播控件
+    private android.support.v4.view.ViewPager mViewPager;
     private TextView mTvPagerTitle;
+    private LinearLayout mLineLayoutDot;
+    private ImageCarousel imageCarousel;
+    private List<View> dots;//小点
 
-    private List<ImageView> mImageList;//轮播的图片集合
-    private String[] mImageTitles;//标题集合
-    private int previousPosition = 0;//前一个被选中的position
-    private List<View> mDots;//小点
+    // 图片数据，包括图片标题、图片链接、数据、点击要打开的网站（点击打开的网页或一些提示指令）
+    private List<ImageInfo> imageInfoList;
 
-    private boolean isStop = false;//线程是否停止
-    private static int PAGER_TIOME = 5000;//间隔时间
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+        imageStart();
 
-    // 在values文件假下创建了pager_image_ids.xml文件，并定义了4张轮播图对应的id，用于点击事件
-    private int[] imgae_ids = new int[]{R.id.pager_image1,R.id.pager_image2,R.id.pager_image3,R.id.pager_image4};
+    }
 
-    /**
-     * 第一步、初始化控件
-     */
-    public void init() {
-        mViewPager = (ViewPager) findViewById(R.id.viewPager);
-        mTvPagerTitle = (TextView) findViewById(R.id.tv_pager_title);
-        initData();//初始化数据
-        initView();//初始化View，设置适配器
-        autoPlayView();//开启线程，自动播放
+    @Override
+    public void onClick(View v) {
+
     }
 
     /**
-     * 第二步、初始化数据（图片、标题、点击事件）
+     * 初始化事件
      */
-    public void initData() {
-        //初始化标题列表和图片
-        mImageTitles = new String[]{"这是一个好看的标题1","这是一个优美的标题2","这是一个快乐的标题3","这是一个开心的标题4"};
-        int[] imageRess = new int[]{R.drawable.ncvt_wifi_head,R.drawable.img1,R.drawable.img2,R.drawable.img3};
+    private void initEvent() {
+        imageInfoList = new ArrayList<>();
+        imageInfoList.add(new ImageInfo(1, "图片1，公告1啦啦啦啦，陆欢博客", "", "http://d.hiphotos.baidu.com/image/pic/item/6159252dd42a2834a75bb01156b5c9ea15cebf2f.jpg", "http://www.cnblogs.com/luhuan/"));
+        imageInfoList.add(new ImageInfo(1, "图片2，公告2啦啦啦啦，陆欢博客", "", "http://c.hiphotos.baidu.com/image/h%3D300/sign=cfce96dfa251f3dedcb2bf64a4eff0ec/4610b912c8fcc3ce912597269f45d688d43f2039.jpg", "http://www.cnblogs.com/luhuan/"));
+        imageInfoList.add(new ImageInfo(1, "图片3，公告3啦啦啦啦，陆欢博客", "", "http://e.hiphotos.baidu.com/image/pic/item/6a600c338744ebf85ed0ab2bd4f9d72a6059a705.jpg", "http://www.cnblogs.com/luhuan/"));
+        imageInfoList.add(new ImageInfo(1, "图片4，公告4啦啦啦啦，陆欢博客", "仅展示", "http://b.hiphotos.baidu.com/image/h%3D300/sign=8ad802f3801001e9513c120f880e7b06/a71ea8d3fd1f4134be1e4e64281f95cad1c85efa.jpg", ""));
+        imageInfoList.add(new ImageInfo(1, "图片5，公告5啦啦啦啦，陆欢博客", "仅展示", "http://e.hiphotos.baidu.com/image/h%3D300/sign=73443062281f95cab9f594b6f9177fc5/72f082025aafa40fafb5fbc1a664034f78f019be.jpg", ""));
 
-        //添加图片到图片列表里
-        mImageList = new ArrayList<>();
-        ImageView iv;
-        for (int i = 0; i < mImageList.size(); i++) {
-            iv = new ImageView(this);
-            iv.setBackgroundResource(imageRess[i]);//设置图片
-            iv.setId(imgae_ids[i]);//顺便给图片设置id
-            iv.setOnClickListener(new pagerImageOnClick());//设置图片点击事件
-            mImageList.add(iv);
+
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
+
+        mViewPager = findViewById(R.id.viewPager);
+        mTvPagerTitle = findViewById(R.id.tv_pager_title);
+        mLineLayoutDot = findViewById(R.id.lineLayout_dot);
+
+    }
+
+    private void imageStart() {
+        //设置图片轮播
+        int[] imgaeIds = new int[]{R.id.pager_image1, R.id.pager_image2, R.id.pager_image3, R.id.pager_image4,
+                R.id.pager_image5, R.id.pager_image6, R.id.pager_image7, R.id.pager_image8};
+        String[] titles = new String[imageInfoList.size()];
+        List<SimpleDraweeView> simpleDraweeViewList = new ArrayList<>();
+
+        for (int i = 0; i < imageInfoList.size(); i++) {
+            titles[i] = imageInfoList.get(i).getTitle();
+            SimpleDraweeView simpleDraweeView = new SimpleDraweeView(this);
+            simpleDraweeView.setAspectRatio(1.78f);
+            // 设置一张默认的图片
+            GenericDraweeHierarchy hierarchy = new GenericDraweeHierarchyBuilder(this.getResources())
+                    .setPlaceholderImage(ContextCompat.getDrawable(this, R.mipmap.banner1), ScalingUtils.ScaleType.CENTER_CROP).build();
+            simpleDraweeView.setHierarchy(hierarchy);
+            simpleDraweeView.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.WRAP_CONTENT));
+
+            //加载高分辨率图片;
+            ImageRequest imageRequest = ImageRequestBuilder.newBuilderWithSource(Uri.parse(imageInfoList.get(i).getImage()))
+                    .setResizeOptions(new ResizeOptions(1280, 720))
+                    .build();
+            PipelineDraweeController controller = (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+                    //.setLowResImageRequest(ImageRequest.fromUri(Uri.parse(listItemBean.test_pic_low))) //在加载高分辨率图片之前加载低分辨率图片
+                    .setImageRequest(imageRequest)
+                    .setOldController(simpleDraweeView.getController())
+                    .build();
+            simpleDraweeView.setController(controller);
+
+            simpleDraweeView.setId(imgaeIds[i]);//给view设置id
+            simpleDraweeView.setTag(imageInfoList.get(i));
+            simpleDraweeView.setOnClickListener(this);
+            titles[i] = imageInfoList.get(i).getTitle();
+            simpleDraweeViewList.add(simpleDraweeView);
+
         }
 
-        //添加轮播点
-        LinearLayout linearLayoutDots = (LinearLayout) findViewById(R.id.lineLayout_dot);
-        mDots = addDots(linearLayoutDots,fromResToDrawable(this,R.drawable.ic_dot_normal),mImageList.size());//其中fromResToDrawable()方法是我自定义的，目的是将资源文件转成Drawable
+        dots = addDots(mLineLayoutDot, fromResToDrawable(this, R.drawable.ic_dot_focused), simpleDraweeViewList.size());
+        imageCarousel = new ImageCarousel(this, mViewPager, mTvPagerTitle, dots, 5000);
+        imageCarousel.init(simpleDraweeViewList, titles)
+                .startAutoPlay();
+        imageCarousel.start();
 
-
-    }
-
-    //图片点击事件
-    private class pagerImageOnClick implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.pager_image1:
-                    Toast.makeText(MainActivity.this, "图片1被点击", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.pager_image2:
-                    Toast.makeText(MainActivity.this, "图片2被点击", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.pager_image3:
-                    Toast.makeText(MainActivity.this, "图片3被点击", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.pager_image4:
-                    Toast.makeText(MainActivity.this, "图片4被点击", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-        }
-    }
-    /**
-     *  第三步、给PagerViw设置适配器，并实现自动轮播功能
-     */
-    public void initView(){
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(mImageList, mViewPager);
-        mViewPager.setAdapter(viewPagerAdapter);
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                //伪无限循环，滑到最后一张图片又从新进入第一张图片
-                int newPosition = position % mImageList.size();
-                // 把当前选中的点给切换了, 还有描述信息也切换
-                mTvPagerTitle.setText(mImageTitles[newPosition]);//图片下面设置显示文本
-                //设置轮播点
-                LinearLayout.LayoutParams newDotParams = (LinearLayout.LayoutParams) mDots.get(newPosition).getLayoutParams();
-                newDotParams.width = 24;
-                newDotParams.height = 24;
-
-                LinearLayout.LayoutParams oldDotParams = (LinearLayout.LayoutParams) mDots.get(previousPosition).getLayoutParams();
-                oldDotParams.width = 16;
-                oldDotParams.height = 16;
-
-                // 把当前的索引赋值给前一个索引变量, 方便下一次再切换.
-                previousPosition = newPosition;
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-        setFirstLocation();
-    }
-
-    /**
-     * 第四步：设置刚打开app时显示的图片和文字
-     */
-    private void setFirstLocation() {
-        mTvPagerTitle.setText(mImageTitles[previousPosition]);
-        // 把ViewPager设置为默认选中Integer.MAX_VALUE / t2，从十几亿次开始轮播图片，达到无限循环目的;
-        int m = (Integer.MAX_VALUE / 2) % mImageList.size();
-        int currentPosition = Integer.MAX_VALUE / 2 - m;
-        mViewPager.setCurrentItem(currentPosition);
-    }
-
-    /**
-     * 第五步: 设置自动播放,每隔PAGER_TIOME秒换一张图片
-     */
-    private void autoPlayView() {
-        //自动播放图片
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (!isStop){
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1);
-                        }
-                    });
-                    SystemClock.sleep(PAGER_TIOME);
-                }
-            }
-        }).start();
-    }
-
-
-
-    /**
-     * 资源图片转Drawable
-     * @param context
-     * @param resId
-     * @return
-     */
-    public Drawable fromResToDrawable(Context context, int resId) {
-        return context.getResources().getDrawable(resId);
     }
 
 
     /**
      * 动态添加一个点
+     *
      * @param linearLayout 添加到LinearLayout布局
-     * @param backgount 设置
-     * @return
+     * @param backgount    设置
+     * @return 小点的Id
      */
-    public int addDot(final LinearLayout linearLayout, Drawable backgount) {
+    private int addDot(final LinearLayout linearLayout, Drawable backgount) {
         final View dot = new View(this);
         LinearLayout.LayoutParams dotParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
         dotParams.width = 16;
         dotParams.height = 16;
-        dotParams.setMargins(4,0,4,0);
+        dotParams.setMargins(4, 0, 4, 0);
         dot.setLayoutParams(dotParams);
         dot.setBackground(backgount);
         dot.setId(View.generateViewId());
-        linearLayout.addView(dot);
+        ((Activity) this).runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                linearLayout.addView(dot);
+            }
+        });
+
         return dot.getId();
+    }
+
+
+    /**
+     * 资源图片转Drawable
+     *
+     * @param context 上下文
+     * @param resId   资源ID
+     * @return 返回Drawable图像
+     */
+    public static Drawable fromResToDrawable(Context context, int resId) {
+        return ContextCompat.getDrawable(context, resId);
+        //return context.getResources().getDrawable(resId);
     }
 
     /**
      * 添加多个轮播小点到横向线性布局
-     * @param linearLayout
-     * @param backgount
-     * @param number
-     * @return
+     *
+     * @param linearLayout 线性横向布局
+     * @param backgount    小点资源图标
+     * @param number       数量
+     * @return 返回小点View集合
      */
-    public List<View> addDots(final LinearLayout linearLayout, Drawable backgount, int number){
+    private List<View> addDots(final LinearLayout linearLayout, Drawable backgount, int number) {
         List<View> dots = new ArrayList<>();
         for (int i = 0; i < number; i++) {
-            int dotId = addDot(linearLayout,backgount);
+            int dotId = addDot(linearLayout, backgount);
             dots.add(findViewById(dotId));
+
         }
         return dots;
-
+    }
 
 }
